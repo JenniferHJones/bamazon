891e28af4f69ad3ab77481788f02c52748b9e594
JJ <jenjones@ucdavis.edu>
2019-03-19 20:31:53 -0700

    update ReadMe & Customer.js file

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # bamazon
+
+## Description
+This Command Line Interface application is meant to imitate Amazon when the customer may place an order that will reduce stock from the store's inventory that is housed in a MySQL database. 
+___
+## User Setup
+1. If necessary, install MySQL on your machine
+2. Clone bamazon
+3. Run NPM Install for the following dependencies
+    * MySQL
+    * Inquirer
+    
+## User Interface
+1. Navigate to where bamazon app is saved, then enter `node bamazonCustomer.js`. 
+2. User will see the store's current inventory. 
+3. User is asked to enter product ID for the item they want to buy. 
+4. User is then asked how many units they wish to buy.
+
+## Technology
+* Node.js
+* NPM MySQL
+* NPM Inquirer
+* JavaScript
+* MySQL Database
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,29 +11,23 @@ var connection = mysql.createConnection({
     database: "bamazon_db"
 });
 
-// connect to the mysql server and sql database
+// connect to mysql server and sql database
 connection.connect(function (err) {
     if (err) throw err;
     displayInventory();
 });
 
+// function to display inventory for user
 function displayInventory() {
     connection.query("SELECT * FROM products", function (err, results) {
         if (err) throw err;
-        // once you have the items, ask user which product they want to buy
+        // ask user which product they want to buy
         console.table(results);
         inquirer
             .prompt([
                 {
                     name: "choice",
                     type: "input",
-                    // choices: function () {
-                    //     var productArray = [];
-                    //     for (var i = 0; i < results.length; i++) {
-                    //         productArray.push(results[i].product_name);
-                    //     }
-                    //     return productArray;
-                    // },
                     message: "Enter the product ID for the item you wish to buy."
                 },
                 {
@@ -42,16 +36,37 @@ function displayInventory() {
                     message: "How many would you like to buy?"
                 }
             ])
-          .then(function(answer) {
-            // get the information of the chosen item
-            var item;
-            for (var i = 0; i < results.length; i++) {
-              if (results[i].item_id === parseInt(answer.choice)) {
-                item = results[i];
-              }
-            }
-            console.log(item);
-        });
+            .then(function (answer) {
+                // get the information for the chosen item
+                var item;
+                for (var i = 0; i < results.length; i++) {
+                    if (results[i].item_id === parseInt(answer.choice)) {
+                        item = results[i];
+                    }
+                }
+                // determine if there is sufficient stock to fill order
+                if (item.stock_quantity >= parseInt(answer.units)) {
+                    var cost = answer.units * item.price;
+                    
+                    console.log(`Congratulations! You bought ${answer.units} ${item.product_name} for $${cost}`);
+
+                    connection.query("UPDATE products SET ? WHERE ?"
+                    [
+                        {
+                            stock_quantity: stock_quantity - answer.units
+                        },
+                        {
+                            item_id: answer.choice
+                        }
+                    ],
+                        function (error) {
+                            if (error) throw error;
+                            displayInventory();
+                        })
+                } else {
+                    console.log('Sorry, inventory is insufficient to meet your order.');
+                }
+            });
         connection.end();
     });
 }
